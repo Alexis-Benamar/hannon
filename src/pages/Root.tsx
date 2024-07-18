eb23f74d133ae424b3b7cdb7f4c83872389bd0c5
Alexis-Benamar <alexis.benamar@viacesi.fr>
2024-07-18 16:25:54 +0200

    add blob-boat.png

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,5 +1,6 @@
 import { Suspense, useContext } from 'react'
 
+import blobBoat from 'assets/blob-boat.png'
 import { AuthContext } from 'context/Auth'
 import { Outlet, useNavigate } from 'react-router-dom'
 
@@ -10,8 +11,12 @@ const Root = () => {
   return (
     <>
       <nav style={{ alignItems: 'center', justifyContent: 'space-between', paddingBlock: '1.5rem' }}>
-        <p className="larger bold pointer" style={{ margin: 0, fontSize: '2rem' }} onClick={() => navigate('/')}>
-          Hannon ⛵
+        <p
+          className="flex center larger bold pointer"
+          style={{ margin: 0, fontSize: '2rem' }}
+          onClick={() => navigate('/')}
+        >
+          Hannon <img src={blobBoat} width={48} height={48} style={{ display: 'inline-block', marginLeft: 10 }} />
         </p>
         {credentials ? (
           <button className="normal" style={{ margin: 0 }} onClick={() => logout()}>
